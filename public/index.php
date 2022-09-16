a533556d61284afb61880df02f14ede7acd1e9ed
Luis Pablo <91643079+Racheth@users.noreply.github.com>
2022-09-16 02:48:38 +0200

    less

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="css/main.css">
+    <title>Panel</title>
+</head>
+<body>
+    <div class="container">
+        <form>
+           
+        <fieldset>
+        <legend>
+        Acceso panel
+        
+        </legend>
+    
+    
+    <label>
+    correo electronico
+    </label>
+    <input type="email" name="">
+    <label>
+    contraseña
+    </label>
+    <input type="password" name="">
+    
+    <button type="submit">
+    A C C E D E R
+        </button>
+</fieldset>
+
+        </form>
+    </div>
+</body>
+</html>
