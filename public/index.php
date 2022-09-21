9ad13e733af155b752b5491ff72b4444985ae557
Luis Pablo <91643079+Racheth@users.noreply.github.com>
2022-09-22 01:42:42 +0200

    login bootstrap

--- public/index.php
+++ public/index.php
@@ -4,35 +4,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  href="css/main.css">
+    <link  href="css/style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>Panel</title>
 </head>
 <body>
-    <div class="container">
-        <form>
-           
-        <fieldset>
-        <legend>
-        Acceso panel
-        
-        </legend>
     
-    
-    <label>
-    correo electronico
-    </label>
-    <input type="email" name="">
-    <label>
-    contraseña
-    </label>
-    <input type="password" name="">
-    
-    <button type="submit">
-    A C C E D E R
-        </button>
-</fieldset>
+        <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
 
-        </form>
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit cum quaerat nihil quae labore. Dolore aliquam, explicabo libero ipsa error pariatur vero veniam voluptates obcaecati? Quidem voluptate facere libero a!</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">correo electronico</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+             
+
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+   
+ 
 </body>
 </html>
