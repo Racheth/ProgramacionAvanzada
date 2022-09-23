37ca6dcbdb90a1b3858c5f32bb2406aace822b91
Luis Pablo <91643079+Racheth@users.noreply.github.com>
2022-09-23 02:58:44 +0200

    trabajo

--- products/index.php
+++ products/index.php
@@ -1,76 +1,137 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-   
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>
+			Products
+		</title>
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+		<style type="text/css">
+			.sidebar{
+				height: 90vh;
+			}
+		</style>
+	</head>
+	<body>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+		<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="#">Navbar</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link active" aria-current="page" href="#">Home</a>
+		        </li>
+		       
+		          <ul class="dropdown-menu">
+		            <li><a class="dropdown-item" href="#">Action</a></li>
+		            <li><a class="dropdown-item" href="#">Another action</a></li>
+		            <li><hr class="dropdown-divider"></li>
+		            <li><a class="dropdown-item" href="#">Something else here</a></li>
+		          </ul>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link disabled">Disabled</a>
+		        </li>
+		      </ul>
+		      <form class="d-flex" role="search">
+		        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+		        <button class="btn btn-outline-success" type="submit">Search</button>
+		      </form>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container-fluid">
+			
+			<div class="row">
+				
+				<div class="col-sm-2 d-sm-block d-none text-bg-dark sidebar">
+					
+					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+			          <li class="nav-item">
+			            <a class="nav-link active" aria-current="page" href="#">Home</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="#">Link</a>
+			          </li>
+			          <li class="nav-item dropdown">
+			            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			              Dropdown
+			            </a>
+			            <ul class="dropdown-menu dropdown-menu-dark">
+			              <li><a class="dropdown-item" href="#">Action</a></li>
+			              <li><a class="dropdown-item" href="#">Another action</a></li>
+			              <li>
+			                <hr class="dropdown-divider">
+			              </li>
+			              <li><a class="dropdown-item" href="#">Something else here</a></li>
+			            </ul>
+			          </li>
+			        </ul>	
+
+				</div>
+
+        <div class = "col-lg-10 col-sm-12">
+          <div class="continer-fluid">
+              <div class = "row">
+                  <div class = "col">
+                      <h4>Productos</h4>
+                  </div>
+                  <div class = "col">
+                      <button class="btn btn-primary float-end">Añadir Productos</button>
+                  </div>
+              </div>
+            </div>
+            <div class ="row">
+            <?php for($i=0; $i < 12; $i++){ ?>
+              <div class="col-md-3 p-2">
+              <div class="card" style="width: 18rem;">
+                 
+                 <img src="producto.png" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Card title</h5>
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                 <a href="#" class="btn btn-primary">Editar</a>
+                 <a href="#" class="btn btn-danger">Eliminar</a>
+                 <a href="#" class="btn btn-primary">Añadir</a>
+               </div>
+             <?php }?>
+           </div>
+              </div>
+            </div>
+      </div>
+    </div>
   </div>
-</nav>
 
-<div class="container-fluid">
-<div class="row">
-<div class="col-sm-2 d-sm-block d-none text-bg-darck sidebar ">
-<ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-</ul>
-</div>
-<div class="col">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate esse aliquam id praesentium voluptas labore optio doloremque vitae sint dolor iure eaque, placeat eligendi explicabo? Libero autem at dicta modi fugit facilis, tempore nihil accusantium ullam eum iusto maiores itaque praesentium vitae error molestias voluptatibus inventore quidem earum quibusdam sequi.
-                <br>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione praesentium repellendus amet sequi ad fugit quaerat eius reiciendis incidunt placeat. Dolorem minima magnam dignissimos aperiam qui ipsum odio soluta quaerat.
-                <div class="row">
+				</div>
+        
 
+			</div>
+
+		</div>
+    <div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
-  
-</body>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+	</body>
 </html>
