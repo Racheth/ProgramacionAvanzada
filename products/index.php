b58f99339ab2a1067346a26e9f9943182bb509b4
Luis Pablo <91643079+Racheth@users.noreply.github.com>
2022-09-29 21:08:36 +0200

    correccion

--- products/index.php
+++ products/index.php
@@ -1,61 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>
-			Products
-		</title>
-		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
-		<style type="text/css">
-			.sidebar{
-				height: 90vh;
-			}
-		</style>
+<?php include('layouts/head.templade.php')
+?>
 	</head>
 	<body>
 
+		<!-- NAVBAR -->
 		<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
-		  <div class="container-fluid">
-		    <a class="navbar-brand" href="#">Navbar</a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
-		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-		        <li class="nav-item">
-		          <a class="nav-link active" aria-current="page" href="#">Home</a>
-		        </li>
-		       
-		          <ul class="dropdown-menu">
-		            <li><a class="dropdown-item" href="#">Action</a></li>
-		            <li><a class="dropdown-item" href="#">Another action</a></li>
-		            <li><hr class="dropdown-divider"></li>
-		            <li><a class="dropdown-item" href="#">Something else here</a></li>
-		          </ul>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link disabled">Disabled</a>
-		        </li>
-		      </ul>
-		      <form class="d-flex" role="search">
-		        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-		        <button class="btn btn-outline-success" type="submit">Search</button>
-		      </form>
-		    </div>
-		  </div>
+		  <?php include('layouts/nav.templade.php')
+		  ?>
 		</nav>
+		<!-- NAVBAR -->
 
 		<div class="container-fluid">
 			
 			<div class="row">
 				
-				<div class="col-sm-2 d-sm-block d-none text-bg-dark sidebar">
+				<!-- SIDEBAR -->
+				<div class="col-sm-2 d-sm-block d-none bg-light sidebar">
 					
 					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-			          <li class="nav-item">
-			            <a class="nav-link active" aria-current="page" href="#">Home</a>
-			          </li>
+			           
 			          <li class="nav-item">
 			            <a class="nav-link" href="#">Link</a>
 			          </li>
@@ -75,63 +41,129 @@
 			        </ul>	
 
 				</div>
+				<!-- SIDEBAR -->
 
-        <div class = "col-lg-10 col-sm-12">
-          <div class="continer-fluid">
-              <div class = "row">
-                  <div class = "col">
-                      <h4>Productos</h4>
-                  </div>
-                  <div class = "col">
-                      <button class="btn btn-primary float-end">Añadir Productos</button>
-                  </div>
-              </div>
-            </div>
-            <div class ="row">
-            <?php for($i=0; $i < 12; $i++){ ?>
-              <div class="col-md-3 p-2">
-              <div class="card" style="width: 18rem;">
-                 
-                 <img src="producto.png" class="card-img-top" alt="...">
-               <div class="card-body">
-                 <h5 class="card-title">Card title</h5>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Editar</a>
-                 <a href="#" class="btn btn-danger">Eliminar</a>
-                 <a href="#" class="btn btn-primary">Añadir</a>
-               </div>
-             <?php }?>
-           </div>
-              </div>
-            </div>
-      </div>
-    </div>
-  </div>
+				<div class="col-md-10 col-lg-10 col-sm-12">
 
+					<section> 
+						<div class="row bg-light m-2">
+							<div class="col">
+								<label>
+									/Productos
+								</label>
+							</div>
+							<div class="col">
+								<button data-bs-toggle="modal" data-bs-target="#addProductModal" class=" float-end btn btn-primary">
+									Añadir producto
+								</button>
+							</div>
+						</div> 
+					</section>
+					
+					<section>
+						
+						<div class="row">
+							
+							<?php for ($i=0; $i < 12; $i++): ?>
+
+							<div class="col-md-4 col-sm-12"> 
+
+								<div class="card mb-2">
+								  <img src="../products/img/fresita.jpg" class="card-img-top" alt="...">
+								  <div class="card-body">
+								    <h5 class="card-title">Card title</h5>
+								    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+								    <div class="row">
+									    <a data-bs-toggle="modal" data-bs-target="#addProductModal" href="#" class="btn btn-warning mb-1 col-6">
+									    	Editar
+									    </a>
+									    <a onclick="eliminar(this)" href="#" class="btn btn-danger mb-1 col-6">
+									    	Eliminar
+									    </a>
+									    <a href="#" class="btn btn-info col-12">
+									    	Detalles
+									    </a>
+								    </div>
+
+								  </div>
+								</div>  
+
+							</div>
+
+							<?php endfor; ?>
+
+						</div>
+
+					</section> 
+
+					 
 				</div>
-        
 
 			</div>
 
 		</div>
-    <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+
+		<!-- Modal -->
+		<div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div class="modal-dialog">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		      </div>
+
+		      <form>
+
+			      <div class="modal-body">
+			        
+			        <?php for ($i=0; $i < 6; $i++): ?>
+			        <div class="input-group mb-3">
+					  <span class="input-group-text" id="basic-addon1">@</span>
+					  <input required type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+					<?php endfor; ?>
+
+			      </div>
+
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+			        	Close
+			        </button>
+			        <button type="submit" class="btn btn-primary">
+			        	Save changes
+			        </button>
+			      </div>
+
+		      </form>
+
+		    </div>
+		  </div>
+		</div>
 
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+		<script type="text/javascript">
+			function eliminar(target)
+			{
+				swal({
+				  title: "Are you sure?",
+				  text: "Once deleted, you will not be able to recover this imaginary file!",
+				  icon: "warning",
+				  buttons: true,
+				  dangerMode: true,
+				})
+				.then((willDelete) => {
+				  if (willDelete) {
+				    swal("Poof! Your imaginary file has been deleted!", {
+				      icon: "success",
+				    });
+				  } else {
+				    swal("Your imaginary file is safe!");
+				  }
+				});
+			}
+		</script>
 	</body>
 </html>
